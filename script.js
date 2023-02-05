330989aa954153e2054ff995feff738bebd44afb
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-02-05 19:27:55 +0530

    docs: ex02

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dob = document.querySelector("#dob");
 const luckyNumber = document.querySelector("#lucky-number");
 const button = document.querySelector(".button");
+const message = document.querySelector(".message");
 
 button.addEventListener("click", () => {
   //   console.log("DateOfBirth :", dob.value);
@@ -9,10 +10,21 @@ button.addEventListener("click", () => {
   date = date.replaceAll("-", "");
   date = Number(date);
   let sum = 0;
-
+  // Calculate sum
   while (date != 0) {
     sum = sum + (date % 10);
     date = Math.floor(date / 10);
   }
-  console.log(sum);
+  //   console.log(sum);
+
+  // Check If lucky
+  if (sum % Number(luckyNumber.value) === 0) {
+    showMsg("Your birthday is lucky");
+  } else {
+    showMsg("Your birthday is NOT lucky, so sad");
+  }
 });
+
+function showMsg(msg) {
+    message.innerText = msg;
+}
